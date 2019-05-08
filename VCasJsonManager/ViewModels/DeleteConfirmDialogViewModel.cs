18062dbd0e7d5b216141bf50674a7a407c965a73
toma-to <23054830+toma-to@users.noreply.github.com>
2019-05-08 20:06:55 +0900

    削除確認ダイアログを追加

---
+++ VCasJsonManager/ViewModels/DeleteConfirmDialogViewModel.cs
@@ -0,0 +1,40 @@
+﻿//
+// VCasJsonManager
+// Copyright 2019 TOMA
+// MIT License
+//
+
+using Livet;
+using Livet.Messaging.Windows;
+
+namespace VCasJsonManager.ViewModels
+{
+    /// <summary>
+    /// 削除確認ダイアログのViewModel
+    /// </summary>
+    public class DeleteConfirmDialogViewModel : ViewModel
+    {
+        /// <summary>
+        /// ダイアログの確認結果
+        /// </summary>
+        public bool Confirmed { get; set; } = false;
+
+        /// <summary>
+        /// OK処理
+        /// </summary>
+        public void Ok()
+        {
+            Confirmed = true;
+            Messenger.Raise(new WindowActionMessage(WindowAction.Close, "Close"));
+        }
+
+        /// <summary>
+        /// キャンセル処理
+        /// </summary>
+        public void Cancel()
+        {
+            Confirmed = false;
+            Messenger.Raise(new WindowActionMessage(WindowAction.Close, "Close"));
+        }
+    }
+}
---
+++ VCasJsonManager/Views/DeleteConfirmDialog.xaml.cs
@@ -0,0 +1,20 @@
+﻿//
+// VCasJsonManager
+// Copyright 2019 TOMA
+// MIT License
+//
+using System.Windows;
+
+namespace VCasJsonManager.Views
+{
+    /// <summary>
+    /// DeleteConfirmDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DeleteConfirmDialog : Window
+    {
+        public DeleteConfirmDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
